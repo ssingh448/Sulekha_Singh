1d5db3cc82ade57752fe9ef902d58941f09092a6
ssingh448 <128328556+ssingh448@users.noreply.github.com>
2024-08-29 21:26:14 -0400

    Update Portfolio.js

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -40,7 +40,7 @@ const Portfolio = () => {
 
                         <div className='navlink'>
 
-                            <a href="/#" active={filter === ''} onClick={() => setFilter('')}>
+                            <a href="/Sulekha_Singh/#" active={filter === ''} onClick={() => setFilter('')}>
                                 <button className='btn_shadow'>
                                     <h2> ALL </h2>
                                     {/*<i className="fab fa-github"></i>*/}
